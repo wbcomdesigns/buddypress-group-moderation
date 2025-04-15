bfb9681c18dfb0c66a9bd46a75102ef9ba77c3a3
Nitin Patil <pallavi@wbcomdesigns.com>
2025-04-15 11:17:11 +0530

    Text domain fixes

--- admin/wbcom/templates/wbcom-support-page.php
+++ admin/wbcom/templates/wbcom-support-page.php
@@ -6,13 +6,13 @@
 	<div class="wbcom-wrap wbcom-plugin-wrapper">
 		<div class="wbcom_admin_header-wrapper">
 			<div id="wb_admin_plugin_name">
-				<?php esc_html_e( 'Support', 'bp-business-profile' ); ?>
+				<?php esc_html_e( 'Support', 'bp-group-moderation' ); ?>
 			</div>
 			<?php echo do_shortcode( '[wbcom_admin_setting_header]' ); ?>
 		</div>        
 		<div id="wbcom_admin_content_support" class="wbcom-all-addons-plugins-wrap">
-			<h4 class="wbcom-support-section"><?php esc_html_e( 'How can we help you?', 'bp-business-profile' ); ?></h4>
-			<p><?php esc_html_e( 'Our team is here to help you out at anytime. If you have any idea about how we could improve. You can share access to your site on our helpdesk if it can help getting faster.', 'bp-business-profile' ); ?></p>
+			<h4 class="wbcom-support-section"><?php esc_html_e( 'How can we help you?', 'bp-group-moderation' ); ?></h4>
+			<p><?php esc_html_e( 'Our team is here to help you out at anytime. If you have any idea about how we could improve. You can share access to your site on our helpdesk if it can help getting faster.', 'bp-group-moderation' ); ?></p>
 
 			<ul id="wbcom_support_list" class="wbcom_boxes_list">
 				<li class="wbcom_help_support_box">
@@ -21,11 +21,11 @@
 					</div>
 					<div class="wbcom_help_support_details">
 						<div class="wbcom_help_support_inner_details">
-							<h4><?php esc_html_e( 'Tickets Support', 'bp-business-profile' ); ?></h4>
-							<p><?php esc_html_e( 'Open a ticket on our helpdesk, we don\'t guarantee a fast response but within a week. Except if you\'ve purchased one of our product we\'ll reply within 24 hours.', 'bp-business-profile' ); ?></p>
+							<h4><?php esc_html_e( 'Tickets Support', 'bp-group-moderation' ); ?></h4>
+							<p><?php esc_html_e( 'Open a ticket on our helpdesk, we don\'t guarantee a fast response but within a week. Except if you\'ve purchased one of our product we\'ll reply within 24 hours.', 'bp-group-moderation' ); ?></p>
 							<div class="wbcom_help_support_button">
 								<a href="https://support.wbcomdesigns.com/portal/newticket" class="wb_btn wb_btn_default" target="_blank">
-									<?php esc_html_e( 'Open a ticket', 'bp-business-profile' ); ?>
+									<?php esc_html_e( 'Open a ticket', 'bp-group-moderation' ); ?>
 								</a>
 							</div>
 						</div>
@@ -38,11 +38,11 @@
 						</div>
 					<div class="wbcom_help_support_details">
 						<div class="wbcom_help_support_inner_details">
-							<h4><?php esc_html_e( 'Contact Us', 'bp-business-profile' ); ?></h4>
-							<p><?php esc_html_e( 'Create a new thread on our plugin page, participation is open to anyone from all around the world. We\'ll be there to help as well but can\'t guarantee any delay.', 'bp-business-profile' ); ?></p>
+							<h4><?php esc_html_e( 'Contact Us', 'bp-group-moderation' ); ?></h4>
+							<p><?php esc_html_e( 'Create a new thread on our plugin page, participation is open to anyone from all around the world. We\'ll be there to help as well but can\'t guarantee any delay.', 'bp-group-moderation' ); ?></p>
 							<div class="wbcom_help_support_button">
 								<a href="https://wbcomdesigns.com/contact/" class="wb_btn wb_btn_default" target="_blank">
-									<?php esc_html_e( 'Contact Us', 'bp-business-profile' ); ?>
+									<?php esc_html_e( 'Contact Us', 'bp-group-moderation' ); ?>
 								</a>
 							</div>
 						</div>
--- admin/wbcom/wbcom-paid-plugin-settings.php
+++ admin/wbcom/wbcom-paid-plugin-settings.php
@@ -27,8 +27,8 @@ if ( ! class_exists( 'Wbcom_Paid_Plugin_Settings' ) ) {
 		public function wbcom_admin_license_page() {
 			add_submenu_page(
 				'wbcomplugins',
-				esc_html__( 'License', 'bp-business-profile' ),
-				esc_html__( 'License', 'bp-business-profile' ),
+				esc_html__( 'License', 'bp-group-moderation' ),
+				esc_html__( 'License', 'bp-group-moderation' ),
 				'manage_options',
 				'wbcom-license-page',
 				array( $this, 'wbcom_license_submenu_page_callback' )
@@ -45,7 +45,7 @@ if ( ! class_exists( 'Wbcom_Paid_Plugin_Settings' ) ) {
 			<li class="wb_admin_nav_item <?php echo esc_attr( $license_page_active ); ?>">
 				<a href="<?php echo esc_url( get_admin_url() ) . 'admin.php?page=wbcom-license-page'; ?>" id="wb_admin_nav_trigger_support">
 					<svg width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M20 22H4C3.44772 22 3 21.5523 3 21V3C3 2.44772 3.44772 2 4 2H20C20.5523 2 21 2.44772 21 3V21C21 21.5523 20.5523 22 20 22ZM19 20V4H5V20H19ZM7 6H11V10H7V6ZM7 12H17V14H7V12ZM7 16H17V18H7V16ZM13 7H17V9H13V7Z"></path></svg>
-					<h4><?php esc_html_e( 'License', 'bp-business-profile' ); ?></h4>
+					<h4><?php esc_html_e( 'License', 'bp-group-moderation' ); ?></h4>
 				</a>
 			</li>
 			<?php
